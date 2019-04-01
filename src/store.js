f9fd10d880e29639a58876f4e86e03cae9762835
speakingcode <dlissner@gmail.com>
2019-04-01 16:28:07 -0500

    Reduce state for SET_MESSAGE_INPU and SUBMIT_MESSAGE actions

--- src/store.js
+++ src/store.js
@@ -11,7 +11,29 @@ import { composeWithDevTools } from 'remote-redux-devtools'
 
 export default () => {
   const store = createStore(
-    (state = {}, action) => action.payload,
+    (
+      state = {
+        messages: [],
+        messageInput: '',
+      },
+      action
+    ) => {
+      switch (action.type) {
+        case 'SET_MESSAGE_INPUT':
+          return {
+            messages: state.messages,
+            messageInput: action.messageInput
+          }
+        case 'SUBMIT_MESSAGE':
+          return {
+            messages: [...state.messages, action.message],
+            messageInput: ''
+          }
+        default:
+          return state
+      }
+      action.payload
+    },
     composeWithDevTools(
       applyMiddleware(
         logger
