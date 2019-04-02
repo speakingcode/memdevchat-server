6af85246cc4e42d6b2c4c2c0641f86df06301aad
speakingcode <dlissner@gmail.com>
2019-04-01 21:07:57 -0500

    Use a selector function to emit only portion/view of state to users

--- src/server.js
+++ src/server.js
@@ -1,17 +1,40 @@
 import Server from 'socket.io'
 
+const selectClientState = (clientId, state) => {
+  return {
+    messages: state.messages.map(msg => ({
+      ...msg,
+      message: (state.users[msg.clientId].username || msg.clientId) + " - " + msg.message
+    })),
+    ...state.users[clientId]
+  }
+}
+
 export default (store, port = 8090) => {
   const connections = {}
   const io = new Server().attach(port)
 
   store.subscribe(
-    () => io.emit('SET_NEW_CLIENT_STATE', store.getState())
+    () => {
+      Object.values(connections).forEach(
+        conn => {
+          conn.emit(
+            'SET_NEW_CLIENT_STATE',
+            selectClientState(conn.id, store.getState())
+          )
+        }
+      )
+    }
   )
 
   io.on('connection', socket => {
     connections[socket.id] = socket
 
-    socket.emit('SET_NEW_CLIENT_STATE', store.getState())
-    socket.on('DISPATCH_CLIENT_ACTION', action => store.dispatch.bind(store)({...action, clientId: socket.id}))
+    socket.emit('SET_NEW_CLIENT_STATE',
+      selectClientState(socket.id, store.getState())
+    )
+    socket.on('DISPATCH_CLIENT_ACTION',
+      action => store.dispatch.bind(store)({...action, clientId: socket.id})
+    )
   })
 }
