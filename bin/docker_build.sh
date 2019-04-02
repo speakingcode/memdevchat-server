35b1c3c7ff5724ce83e842d722fa1ecc25605e35
speakingcode <dlissner@gmail.com>
2019-04-02 13:34:15 -0500

    Script building/running container

---
+++ bin/docker_build.sh
@@ -0,0 +1,6 @@
+echo "Stopping existing memdevchat-server container (if any)..."
+docker container stop memdevchat-server
+echo "Removing memdevchat-server image (if any)..."
+docker image rm memdevchat-server
+echo "Building Dockerfile.dev image with tag 'memdevchat-server'..."
+docker build -f Dockerfile -t memdevchat-server .
---
+++ bin/docker_run.sh
@@ -0,0 +1,6 @@
+#stop and remove server container if running
+docker container stop memdevchat-server
+docker container rm memdevchat-server
+
+#run the container with terminal, name it, mount src dir, bind ports
+docker run -t -i --name memdevchat-server -v `pwd`/src:/memdevchat-server/src -p 8090:8090 memdevchat-server npm start
